b2c7d2c7848be76d23697337800b5c048b61f9da
alvaro-j-perez <63925351+alvaro-j-perez@users.noreply.github.com>
2021-04-11 23:41:00 -0600

    Add files via upload

---
+++ cursos.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ListaAbril
+{
+    public class Cursos
+    {
+        public String nombrecurso;
+        public int nota1;
+        public int nota2;
+        public int final;
+
+        public Cursos(String nombrecurso, int nota1, int nota2, int final)
+        {
+            this.nombrecurso = nombrecurso;
+            this.nota1= nota1;
+            this.nota2 = nota2;
+            this.final= final;
+
+        }
+    }
+}
