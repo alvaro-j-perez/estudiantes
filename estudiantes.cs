5e3187dc62f916d2ed1baa117cddd5f1a413d646
alvaro-j-perez <63925351+alvaro-j-perez@users.noreply.github.com>
2021-04-11 23:42:31 -0600

    Add files via upload

---
+++ estudiantes.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ListaAbril
+{
+    public class Estudiantes
+    {
+        public String nombre;
+        public int edad;
+        public String carnet;
+        public Boolean solvente;
+
+        public Estudiantes(String nombre, int edad, String carnet, Boolean solvente)
+        {
+            this.nombre= nombre;
+            this.edad= edad;
+            this.carnet= carnet;
+            this.solvente= solvente;
+             
+        }  
+        
+    }
+}
